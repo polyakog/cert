0bda2717d5740376e1082f52c2271dbafc0f204a
polyakog <gpolyakov77@gmail.com>
2023-06-01 10:28:01 +0400

    added buttons

--- src/component/freeeCodeCamp/BootstrapTaskFCC.jsx
+++ src/component/freeeCodeCamp/BootstrapTaskFCC.jsx
@@ -9,7 +9,20 @@ export const BootstrapTaskFCC = () => {
             <div className={css.container_fluid}> {/* mobile - responsive */}
                 <h3 className={css.text_primary + ' ' + css.text_center}> jQuery Playground</h3>
                 <div className={css.row}></div>
-                <div class={css.col_xs_6}></div>
+                <div class={css.col_xs_6}>
+                    <div class={css.well}>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                    </div>
+                </div>
+                <div class={css.col_xs_6}>
+                    <div class={css.well}>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                        <button className={css.btn + ' ' + css.btn_default}></button>
+                    </div>
+                </div>
 
             </div>
 
