7e5eb7f95aeae34c4a9784f754ade48f3cbf1301
polyakog <gpolyakov77@gmail.com>
2023-06-01 10:51:19 +0400

    added ids for buttons and well classes

--- src/component/freeeCodeCamp/BootstrapTaskFCC.jsx
+++ src/component/freeeCodeCamp/BootstrapTaskFCC.jsx
@@ -9,18 +9,20 @@ export const BootstrapTaskFCC = () => {
             <div className={css.container_fluid}> {/* mobile - responsive */}
                 <h3 className={css.text_primary + ' ' + css.text_center}> jQuery Playground</h3>
                 <div className={css.row}></div>
-                <div class={css.col_xs_6}>
-                    <div class={css.well}>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
+                <div className={css.col_xs_6}>
+                    <h4>#left-well</h4>
+                    <div class={css.well} id="left-well">
+                        <button className={css.btn + ' ' + css.btn_default + ' ' + css.target} id="target1"></button>
+                        <button className={css.btn + ' ' + css.btn_default} id="target2"></button>
+                        <button className={css.btn + ' ' + css.btn_default} id="target3"></button>
                     </div>
                 </div>
-                <div class={css.col_xs_6}>
-                    <div class={css.well}>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
-                        <button className={css.btn + ' ' + css.btn_default}></button>
+                <div className={css.col_xs_6}>
+                    <h4>#right-well</h4>
+                    <div className={css.well} id="right-well">
+                        <button className={css.btn + ' ' + css.btn_default} id="target4"></button>
+                        <button className={css.btn + ' ' + css.btn_default} id="target5"></button>
+                        <button className={css.btn + ' ' + css.btn_default} id="target6"></button>
                     </div>
                 </div>
 
