43f772bd084b7da66bd90a23910832132160c758
polyakog <gpolyakov77@yandex.ru>
2023-06-16 18:53:16 +0400

    selection in jQuery: nth-child(), even/odd

--- src/component/freeeCodeCamp/jQueryTaskFCC.jsx
+++ src/component/freeeCodeCamp/jQueryTaskFCC.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react"
-import css from './FirstTask.module.css'
+// import css from './FirstTask.module.css'
+import './jQueryTaskFCC.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import $ from "jquery"
@@ -10,56 +11,48 @@ export const JQueryTaskFCC = () => {
     //     console.log('document is ready')
     // });
 
-const onjQueryReady = () => {
-    $("#target1").css("color", "red");
-    $("#target1").prop("disabled", true);
-    $('h3').html('<em>jQuery</em>')
-    $("#target4").remove();
-    $("#target2").appendTo("#right-well")
-    $('#target5').clone().appendTo('#left-well')
-    $('#target1').parent().css('background-color', 'red')
-    $('#right-well').children().css('color', 'orange')
+    const onjQueryReady = () => {
+        $("#target1").css("color", "red");
+        $("#target1").prop("disabled", true);
+        $('h3').html('<em>jQuery</em>')
+        $("#target4").remove();
+        $("#target2").appendTo("#right-well")
+        $('#target5').clone().appendTo('#left-well')
+        $('#target1').parent().css('background-color', 'red')
+        $('#right-well').children().css('color', 'orange')
+        $('.target:nth-child(2)').addClass('bounce')
+        $('button:nth-child(3)').css('color', 'gray')
+        $(".target:even").addClass("shake");
+    }
 
 
-}
-
-    // useEffect(() => {
-
-    //     $(function () {
-    //         $("#target1").css("color", "red");
-    //         $("#target1").prop("disabled", true);
-    //         // $("#target4").remove();
-    //         $("#target2").appendTo("#right-well")
-
-    //     });
-    // }, [])
 
     return (
         <div>
 
 
             {/* Code below this line should not be changed */}
-            <div className={css.container_fluid}> {/* mobile - responsive */}
-                <h3 className={css.text_primary + ' ' + css.text_center}> jQuery Playground-</h3>
-                <div className={css.row}></div>
-                <div className={css.col_xs_6}>
+            <div className={'container_fluid'}> {/* mobile - responsive */}
+                <h3 className={'text_primary text_center'}> jQuery Playground-</h3>
+                <div className={'row'}></div>
+                <div className={'col_xs_6'}>
                     <h4>#left-well</h4>
-                    <div className={css.well} id="left-well">
-                        <button className={css.btn + ' ' + css.btn_default + ' ' + css.target} id="target1">#target1</button>
-                        <button className={css.btn + ' ' + css.btn_default} id="target2">#target2</button>
-                        <button className={css.btn + ' ' + css.btn_default} id="target3">#target3</button>
+                    <div className={'well'} id="left-well">
+                        <button className={'btn btn-default target'} id="target1">#target1</button>
+                        <button className={'btn btn-default target'} id="target2">#target2</button>
+                        <button className={'btn btn-default target'} id="target3">#target3</button>
                     </div>
                 </div>
-                <div className={css.col_xs_6}>
+                <div className={'col_xs_6'}>
                     <h4>#right-well</h4>
-                    <div className={css.well} id="right-well">
-                        <button className={css.btn + ' ' + css.btn_default} id="target4">#target4</button>
-                        <button className={css.btn + ' ' + css.btn_default} id="target5">#target5</button>
-                        <button className={css.btn + ' ' + css.btn_default} id="target6">#target6</button>
+                    <div className={'well'} id="right-well">
+                        <button className={'btn btn-default target'} id="target4">#target4</button>
+                        <button className={'btn btn-default target'} id="target5">#target5</button>
+                        <button className={'btn btn-default target'} id="target6">#target6</button>
                     </div>
                 </div>
 
-                <button onClick={onjQueryReady} style={{ marginTop: '20px', background: '#2e87e38f'}}>jQuery</button>
+                <button onClick={onjQueryReady} style={{ marginTop: '20px', background: '#2e87e38f' }}>jQuery</button>
 
             </div>
 
