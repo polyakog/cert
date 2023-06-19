00082108343169a90a8d2a6901403f8a984127cf
polyakog <gpolyakov77@yandex.ru>
2023-06-19 13:17:19 +0400

    SASS: @for loop

--- src/component/freeeCodeCamp/Sass/Sass.jsx
+++ src/component/freeeCodeCamp/Sass/Sass.jsx
@@ -4,21 +4,27 @@ import './Sass.scss'
 export const SassFCC = () => {
 
     const onButtonClick = () => {
-       
+
     }
+
    
-    
     return (
-       
-            <div> 
-                <h3 > Sass</h3>
 
-                <div id="box"></div>
+        <div>
+            <h3 > Sass</h3>
 
-                <button onClick={onButtonClick} style={{ marginTop: '20px', background: '#2e87e38f' }}>Apply</button>
+            <div id="box"></div>
 
-            </div>
-        
+            <p class="text-1">Hello</p>
+            <p class="text-2">Hello</p>
+            <p class="text-3">Hello</p>
+            <p class="text-4">Hello</p>
+            <p class="text-5">Hello</p>
+
+            <button onClick={onButtonClick} style={{ marginTop: '20px', background: '#2e87e38f' }}>Apply</button>
+
+        </div>
+
     )
 
 }
